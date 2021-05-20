b4b51c841878c2f8b192a7b2fb2a2e3fe34b5a1f
lianjieshan <shanlianjie0304@gmail.com>
2021-05-20 07:36:08 -0400

    sql

---
+++ Expenditures.sql
@@ -0,0 +1,22 @@
+SET IDENTITY_INSERT [dbo].[Expenditures] ON 
+
+INSERT [dbo].[Incomes] (Id, UserId,Amount,Description,IncomeDate,Remarks) VALUES (1,1,1000,N'This is Income', '2020-09-09','complete')
+INSERT [dbo].[Incomes] (Id, UserId,Amount,Description,IncomeDate,Remarks) VALUES (2,1,2000,N'This is Income', '2020-09-10','complete')
+INSERT [dbo].[Incomes] (Id, UserId,Amount,Description,IncomeDate,Remarks) VALUES (3,2,1000,N'This is Income', '2020-09-19','complete')
+INSERT [dbo].[Incomes] (Id, UserId,Amount,Description,IncomeDate,Remarks) VALUES (4,3,1000,N'This is Income', '2020-09-29','complete')
+INSERT [dbo].[Incomes] (Id, UserId,Amount,Description,IncomeDate,Remarks) VALUES (5,4,2000,N'This is Income', '2020-10-09','complete')
+INSERT [dbo].[Incomes] (Id, UserId,Amount,Description,IncomeDate,Remarks) VALUES (6,5,2000,N'This is Income', '2020-10-09','complete')
+INSERT [dbo].[Incomes] (Id, UserId,Amount,Description,IncomeDate,Remarks) VALUES (7,5,3000,N'This is Income', '2020-10-09','complete')
+INSERT [dbo].[Incomes] (Id, UserId,Amount,Description,IncomeDate,Remarks) VALUES (8,6,4000,N'This is Income', '2020-11-09','complete')
+INSERT [dbo].[Incomes] (Id, UserId,Amount,Description,IncomeDate,Remarks) VALUES (9,7,5000,N'This is Income', '2020-11-09','complete')
+INSERT [dbo].[Incomes] (Id, UserId,Amount,Description,IncomeDate,Remarks) VALUES (10,8,1000,N'This is Income', '2020-11-09','complete')
+INSERT [dbo].[Incomes] (Id, UserId,Amount,Description,IncomeDate,Remarks) VALUES (11,9,1000,N'This is Income', '2020-11-09','complete')
+INSERT [dbo].[Incomes] (Id, UserId,Amount,Description,IncomeDate,Remarks) VALUES (12,10,1000,N'This is Income', '2020-11-09','complete')
+INSERT [dbo].[Incomes] (Id, UserId,Amount,Description,IncomeDate,Remarks) VALUES (13,10,1000,N'This is Income', '2020-11-09','complete')
+INSERT [dbo].[Incomes] (Id, UserId,Amount,Description,IncomeDate,Remarks) VALUES (14,8,1000,N'This is Income', '2020-12-09','complete')
+INSERT [dbo].[Incomes] (Id, UserId,Amount,Description,IncomeDate,Remarks) VALUES (15,1,2000,N'This is Income', '2020-12-09','complete')
+INSERT [dbo].[Incomes] (Id, UserId,Amount,Description,IncomeDate,Remarks) VALUES (16,1,3000,N'This is Income', '2020-12-09','complete')
+INSERT [dbo].[Incomes] (Id, UserId,Amount,Description,IncomeDate,Remarks) VALUES (17,1,2000,N'This is Income', '2020-12-09','complete')
+INSERT [dbo].[Incomes] (Id, UserId,Amount,Description,IncomeDate,Remarks) VALUES (18,2,1000,N'This is Income', '2020-12-09','complete')
+
+SET IDENTITY_INSERT [dbo].[Expenditures] OFF
